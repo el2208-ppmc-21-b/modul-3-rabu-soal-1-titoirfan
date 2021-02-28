0cf680f1b3a50e8ed10f77adfa7a75994662ebd6
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-02-28 16:28:15 +0700

    Update ss2tf.c

--- ss2tf.c
+++ ss2tf.c
@@ -1,25 +1,89 @@
-/*  EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul            : 3
-*   Percobaan        : 
-*   Hari dan Tanggal : 
-*   Nama (NIM)       : Andhika Rahadian (13218030) 
-*   Asisten (NIM)    : Irfan Tito Kurniawan (18317019)
-*   Nama File        : ss2tf.c
-*   Deskripsi        : 
-*/
-
 #include <stdio.h>
 #include "ss2tf.h"
+#define asciiFixed 97
 
 void inputMatrixData(float _matA[2][2], float _matB[2][1], float _matC[1][2], float *data, char elmt)
 {
-    // Implementasi Anda
+    int x = (int)elmt;
+    x -= asciiFixed;
+    if (x < 4)
+    {
+        _matA[x / 2][x % 2] = *data;
+    }
+    if (x >= 4 && x < 6)
+    {
+        _matB[x % 4][0] = *data;
+    }
+    if (x >= 6)
+    {
+        _matC[0][x % 6] = *data;
+    }
 }
 void printNumerator(float *a, float *b, float *c, float *d, float *e, float *f, float *g, float *h)
 {
-    // Implementasi Anda
+    float m = *e * *g + *h * *f;
+    float n = -1 * ((*e) * (*d) * (*g) - (*h) * (*c) * (*e) - (*b) * (*g) * (*f) + (*h) * (*a) * (*f));
+    // Ada komponen s
+    if (m < 0)
+    {
+        m = -1 * m;
+        printf(" - %.2fs", m);
+    }
+    else if (m > 0)
+    {
+        printf("%.2fs", m);
+    }
+
+    // Tidak ada komponen s
+    if (m == 0)
+    {
+        if (n < 0)
+        {
+            n = -1 * n;
+            printf(" - %.2f", n);
+        }
+        else if (n > 0)
+        {
+            printf("%.2f", n);
+        }
+    }
+    else
+    {
+        if (n < 0)
+        {
+            n = -1 * n;
+            printf(" - %.2f", n);
+        }
+        else if (n > 0)
+        {
+            printf(" + %.2f", n);
+        }
+    }
 }
 void printDenominator(float _matA[2][2], float _matB[2][1], float _matC[1][2])
 {
-    // Implementasi Anda
+    printf("s^2");
+    float b = -1 * (_matA[0][0] + _matA[1][1]);
+    float c = (_matA[0][0] * _matA[1][1]) - (_matA[1][0] * _matA[0][1]);
+
+    // Ketika b negatif, b harus dipositifkan agar dapat di printf negatif
+    if (b < 0)
+    {
+        b = -1 * b;
+        printf(" - %.2fs", b);
+    }
+    else if (b > 0)
+    {
+        printf(" + %.2fs", b);
+    }
+
+    if (c < 0)
+    {
+        c = -1 * c;
+        printf(" - %.2f", c);
+    }
+    else if (c > 0)
+    {
+        printf(" + %.2f", c);
+    }
 }

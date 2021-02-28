3039225f5ca9c73d850fd051b7118467368eedf2
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-02-28 16:27:43 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,43 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul               : 3
-*   Soal                : 1
-*   Hari dan Tanggal    : 
-*   Nama (NIM)          : 
-*   Asisten (NIM)       : 
-*   Nama File           : main.c
-*   Deskripsi           : Deskripsi file ini.
+/*  EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
+*   Modul            : 3
+*   Percobaan        : -
+*   Hari dan Tanggal : -
+*   Nama (NIM)       : - 
+*   Asisten (NIM)    : Andhika Rahadian (13218030)
+*   Nama File        : -
+*   Deskripsi        : Program Utama Translasi StateSpace -> Transfer Function
 */
 
 #include <stdio.h>
 #include "ss2tf.h"
+// MAKEFILE SCRIPT :
+// gcc -o main main.c ss2tf.c -lm
+
+#define MAX_DATA 8
+#define asciiFixed 97
 
 int main()
 {
-    // Deklarasi data
+    // Deklarasi dan Persiapan matriks State Space
     float matrixA[2][2], matrixB[2][1], matrixC[1][2];
+    // Untuk kebutuhan Increment character
+    int asciiTemp = asciiFixed;
+    float dataBuf = 0;
 
-    // Input Data Matriks satu per satu
-    inputMatrixData(...., ....);
+    // Input Matrix Data one by one
+    for (int i = 0; i < MAX_DATA; ++i)
+    {
+        printf("Masukkan nilai %c : ", asciiTemp);
+        scanf("%f", &dataBuf);
+        inputMatrixData(matrixA, matrixB, matrixC, &dataBuf, (char)asciiTemp);
 
-    // Cetak Transfer Function (jangan diubah agar tidak salah autograde)
+        ++asciiTemp;
+    }
+
+    // Cetak Hasil Transfer Function
     printf("Transfer Function:\n\n");
-
-    // Cetak Pembilang
-    printNumerator(...., ....);
-
-    // ----- untuk "per" pada pecahan (jangan diubah agar tidak salah autograde)
+    printNumerator(&matrixA[0][0], &matrixA[0][1], &matrixA[1][0], &matrixA[1][1], &matrixB[0][0], &matrixB[1][0], &matrixC[0][0], &matrixC[0][1]);
     printf("\n--------------------\n");
+    printDenominator(matrixA, matrixB, matrixC);
 
-    // Cetak Penyebut
-    printDenominator(...., ....);
-
-    return 0;
 }
